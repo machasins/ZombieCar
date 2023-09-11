8f70998c7f0ac87799d2a6dc03c204effaa61611
LuisMint <62115750+LuisMint@users.noreply.github.com>
2023-09-11 16:56:15 -0700

    SHADER AND OTHER STUFF

---
+++ Assets/Art testing/Car Models/BreathingEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathingEffect : MonoBehaviour
+{
+    public float minScale = 0.9f;       // Minimum scale value
+    public float maxScale = 1.1f;       // Maximum scale value
+    public float breathSpeed = 1.0f;    // Breathing speed
+    private Vector3 originalScale;      // The original scale of the object
+
+    private void Start()
+    {
+        // Store the original scale of the object
+        originalScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        // Calculate the new scale using a sine wave to create a breathing effect
+        float scaleFactor = Mathf.Lerp(minScale, maxScale, Mathf.PingPong(Time.time * breathSpeed, 1.0f));
+
+        // Apply the new scale to the object
+        transform.localScale = originalScale * scaleFactor;
+    }
+}
