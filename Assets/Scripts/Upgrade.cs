18600722f8ca2312c49101234f76208fd58ba797
Michael Kinyon <michaelkinyon@gmail.com>
2023-09-12 18:47:08 -0700

    Upgrade menu done for now

--- Assets/Scripts/Upgrade.cs
+++ Assets/Scripts/Upgrade.cs
@@ -5,14 +5,14 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(CarController))]
-[RequireComponent(typeof(Health))]
 public class Upgrade : MonoBehaviour
 {
     [Header("Upgrades are additive.")]
     [Header("Any Fields that should not be changed, set to NaN.")]
     public bool doCarController;
     public bool doHealth;
+    public string title = "";
+    [TextArea()] public string description = "";
     private CarController carController;
     private Health health;
 
@@ -20,11 +20,17 @@ public class Upgrade : MonoBehaviour
 
     private void Awake()
     {
-        carController = GetComponent<CarController>();
-        health = GetComponent<Health>();
+        if (doCarController)
+        {
+            carController = GetComponent<CarController>();
+            carController.enabled = false;
+        }
 
-        carController.enabled = false;
-        health.enabled = false;
+        if (doHealth)
+        {
+            health = GetComponent<Health>();
+            health.enabled = false;
+        }
 
         if (wcr == null)
             wcr = FindObjectOfType<WorldComponentReference>();
@@ -32,7 +38,7 @@ public class Upgrade : MonoBehaviour
 
     private void OnEnable()
     {
-        if (doCarController)
+        if (doCarController && wcr.player)
         {
             CarController playerCar = wcr.player.GetComponent<CarController>();
             foreach (FieldInfo prop in playerCar.GetType().GetFields())
@@ -47,7 +53,7 @@ public class Upgrade : MonoBehaviour
             }
         }
 
-        if (doHealth)
+        if (doHealth && wcr.player)
         {
             Health playerCar = wcr.player.GetComponent<Health>();
             foreach (FieldInfo prop in playerCar.GetType().GetFields())
@@ -65,7 +71,7 @@ public class Upgrade : MonoBehaviour
 
     private void OnDisable()
     {
-        if (doCarController)
+        if (doCarController && wcr.player)
         {
             CarController playerCar = wcr.player.GetComponent<CarController>();
             foreach (FieldInfo prop in carController.GetType().GetFields())
@@ -80,7 +86,7 @@ public class Upgrade : MonoBehaviour
             }
         }
 
-        if (doHealth)
+        if (doHealth && wcr.player)
         {
             Health playerCar = wcr.player.GetComponent<Health>();
             foreach (FieldInfo prop in health.GetType().GetFields())
--- Assets/UpgradeMenuHandler.cs
+++ Assets/UpgradeMenuHandler.cs
@@ -2,55 +2,141 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+using Unity.VisualScripting;
+using System;
 
 public class UpgradeMenuHandler : MonoBehaviour
 {
     public GameObject playerUpgrades;
     public GameObject equipmentMenu;
-
+    [Space()]
+    public TMP_Text menuText;
+    public TMP_Text equipedText;
+    public TMP_Text descText;
+    [Space()]
     public GameObject buttonPrefab;
-}
+    [Space()]
+    public int maxWidth;
+    public Vector2 offsets;
 
-[CustomEditor(typeof(UpgradeMenuHandler))]
-public class UpgradeMenuHandlerEditor : Editor
-{
-    public SerializedProperty playerUpgrades;
-    public SerializedProperty equipmentMenu;
-    public SerializedProperty buttonPrefab;
-
-    private void OnEnable()
+    private void Start()
     {
-        playerUpgrades = serializedObject.FindProperty("playerUpgrades");
-        equipmentMenu = serializedObject.FindProperty("equipmentMenu");
-        buttonPrefab = serializedObject.FindProperty("buttonPrefab");
+        menuText.text = "Equipment";
+        equipedText.text = "";
+        descText.text = "";
+
+        GameObject upgradeContainer = Instantiate(new GameObject(), equipmentMenu.transform);
+        upgradeContainer.name = "Equipment";
+
+        int upgradeType = 0;
+        Vector2 offset = Vector2.zero;
+        foreach (Transform type in playerUpgrades.transform)
+        {
+            CreateMainMenuButton(upgradeContainer.transform, type, ref offset, upgradeType);
+            ++upgradeType;
+        }
+
+        CreateExitButton(upgradeContainer.transform, offset);
     }
 
-    public override void OnInspectorGUI()
+    private void CreateMainMenuButton(Transform parent, Transform upgradeType, ref Vector2 offset, int upgradeIndex)
     {
-        DrawDefaultInspector();
+        // Main Menu Button
+        GameObject mainMenuButton = Instantiate(buttonPrefab, parent);
+        mainMenuButton.name = upgradeType.name;
+        mainMenuButton.GetComponentInChildren<TMP_Text>().text = upgradeType.name;
+        mainMenuButton.GetComponent<RectTransform>().anchoredPosition = offset;
 
-        if (equipmentMenu.objectReferenceValue
-            && playerUpgrades.objectReferenceValue
-            && buttonPrefab.objectReferenceValue
-            && GUILayout.Button("Remake Buttons"))
+        offset = ((offset.x + offsets.x) / (maxWidth * offsets.x) < 1.0f)
+            ? new Vector2(offset.x + offsets.x, offset.y)
+            : new Vector2(0, offset.y + offsets.y);
+
+        // Sub Menu Buttons
+        GameObject subMenuContainer = Instantiate(new GameObject(), equipmentMenu.transform);
+        subMenuContainer.name = upgradeType.name;
+
+        int index = 0;
+        Vector2 currOffset = Vector2.zero;
+        foreach (Transform upgrade in upgradeType)
         {
-            GameObject menu = equipmentMenu.objectReferenceValue as GameObject;
-            GameObject player = playerUpgrades.objectReferenceValue as GameObject;
-            GameObject button = buttonPrefab.objectReferenceValue as GameObject;
-
-            //foreach (Transform t in menu.transform)
-            //    DestroyImmediate(t.gameObject);
-
-            foreach (Transform type in player.transform)
-            {
-                GameObject t = Instantiate(new GameObject(), menu.transform);
-                t.name = type.name;
-                foreach (Transform upgrade in type)
-                {
-                    GameObject u = Instantiate(button, t.transform);
-                    u.name = upgrade.name;
-                }
-            }
+            CreateSubMenuButton(upgrade, subMenuContainer.transform, ref currOffset, new(upgradeIndex, index));
+            ++index;
         }
+
+        CreateSubBackButton(subMenuContainer.transform, parent.gameObject, currOffset);
+
+        // Variables for button
+        Transform currUpgrade = upgradeType.GetChild(playerUpgrades.GetComponentInParent<UpgradeSystem>().GetUpgrade(upgradeIndex));
+        Upgrade upgradeInfo = currUpgrade.GetComponent<Upgrade>();
+        string title = (upgradeInfo && upgradeInfo.title != "") ? upgradeInfo.title : currUpgrade.name;
+        string desc = (upgradeInfo && upgradeInfo.description != "") ? upgradeInfo.description : "##Description not found##";
+
+        // Set up Main Menu Button
+        Button b = mainMenuButton.GetComponent<Button>();
+        b.onClick.AddListener(delegate { parent.gameObject.SetActive(false); });
+        b.onClick.AddListener(delegate { subMenuContainer.SetActive(true); });
+        b.onClick.AddListener(delegate { menuText.text = subMenuContainer.name; });
+        b.onClick.AddListener(delegate { equipedText.text = title; });
+        b.onClick.AddListener(delegate { descText.text = desc; });
+
+        subMenuContainer.SetActive(false);
+    }
+
+    private void CreateSubMenuButton(Transform upgrade, Transform parent, ref Vector2 currOffset, Vector2Int upgradeIndex)
+    {
+        // Sub Menu Button
+        GameObject u = Instantiate(buttonPrefab, parent);
+        u.name = upgrade.name;
+        u.GetComponentInChildren<TMP_Text>().text = upgrade.name;
+        u.GetComponent<RectTransform>().anchoredPosition = currOffset;
+
+        currOffset = ((currOffset.x + offsets.x) / (maxWidth * offsets.x) < 1.0f)
+            ? new Vector2(currOffset.x + offsets.x, currOffset.y)
+            : new Vector2(0, currOffset.y + offsets.y);
+
+        // Variables for Button
+        Upgrade upgradeInfo = upgrade.GetComponent<Upgrade>();
+        string title = (upgradeInfo && upgradeInfo.title != "") ? upgradeInfo.title : u.name;
+        string desc = (upgradeInfo && upgradeInfo.description != "") ? upgradeInfo.description : "##Description not found##";
+
+        // Set up Sub Menu Button
+        Button sub = u.GetComponent<Button>();
+        sub.onClick.AddListener(delegate { playerUpgrades.GetComponentInParent<UpgradeSystem>().ChangeUpgrade(upgradeIndex.x, upgradeIndex.y); });
+        sub.onClick.AddListener(delegate { equipedText.text = title; });
+        sub.onClick.AddListener(delegate { descText.text = desc; });
+    }
+
+    private void CreateSubBackButton(Transform parent, GameObject equip, Vector2 currOffset)
+    {
+        // Back button Button
+        GameObject bb = Instantiate(buttonPrefab, parent);
+        bb.name = "Back";
+        bb.GetComponentInChildren<TMP_Text>().text = "Back";
+        bb.GetComponent<RectTransform>().anchoredPosition = currOffset;
+        bb.GetComponent<Image>().color = Color.white;
+
+        // Set up Back Button
+        Button b = bb.GetComponent<Button>();
+        b.onClick.AddListener(delegate { equip.SetActive(true); });
+        b.onClick.AddListener(delegate { parent.gameObject.SetActive(false); });
+        b.onClick.AddListener(delegate { menuText.text = "Equipment"; });
+        b.onClick.AddListener(delegate { equipedText.text = ""; });
+        b.onClick.AddListener(delegate { descText.text = ""; });
+    }
+
+    private void CreateExitButton(Transform parent, Vector2 offset)
+    {
+        // Exit button Button
+        GameObject exit = Instantiate(buttonPrefab, parent);
+        exit.name = "Exit";
+        exit.GetComponentInChildren<TMP_Text>().text = "Exit";
+        exit.GetComponent<RectTransform>().anchoredPosition = offset;
+        exit.GetComponent<Image>().color = Color.white;
+
+        // Set up Exit Button
+        exit.GetComponent<Button>().onClick.AddListener(delegate { FindObjectOfType<WorldComponentReference>().ToggleUpgradeUI(false); });
     }
 }
