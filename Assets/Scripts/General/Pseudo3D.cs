e7d02f2214d9a9498f53a0b88c64ada521817f00
Michael Kinyon <michaelkinyon@gmail.com>
2023-09-19 19:51:20 -0700

    fixed pseudo 3d

--- Assets/Scripts/General/Pseudo3D.cs
+++ Assets/Scripts/General/Pseudo3D.cs
@@ -4,37 +4,41 @@ using UnityEngine;
 
 public class Pseudo3D : MonoBehaviour
 {
-    public Transform topBone;
     public float maximumRadius;
 
     private Transform mainCamera;
+    private List<Transform> topBone = new();
+    private List<Transform> topParent = new();
+    private List<Vector3> originalOffset = new();
 
     private void Awake()
     {
-        if (topBone == null)
+        foreach (Transform t in GetComponentsInChildren<Transform>())
         {
-            foreach (Transform t in GetComponentsInChildren<Transform>())
+            if (t.gameObject.name.Contains("TOP"))
             {
-                if (t.gameObject.name.Contains("TOP"))
-                {
-                    topBone = t;
-                    // set values based on name;
-                    break;
-                }
+                topBone.Add(t);
+                topParent.Add(t.parent);
+                originalOffset.Add(t.position - t.parent.position);
+                // set values based on name;
             }
-
-            if (topBone == null)
-                Debug.LogError("Pseudo3D cannot find TOP bone on object " + name);
         }
 
-        mainCamera = FindObjectOfType<WorldComponentReference>().cameraPosition.transform;
+        if (topBone.Count <= 0)
+            Debug.LogError("Pseudo3D cannot find TOP bone on object " + name);
+
+    mainCamera = FindObjectOfType<WorldComponentReference>().cameraPosition.transform;
     }
 
     private void Update()
     {
-        Vector2 awayAngle = (transform.position - mainCamera.position).normalized;
-        float distance = Vector2.Distance(transform.position, mainCamera.position);
+        for (int i = 0; i < topBone.Count; ++i)
+        {
+            Vector2 bonePosition = (Vector2)(topParent[i].position + originalOffset[i]);
+            Vector2 awayAngle = (bonePosition - (Vector2)mainCamera.position).normalized;
+            float distance = Vector2.Distance(bonePosition, mainCamera.position);
 
-        topBone.position = (Vector2)transform.position + awayAngle * Mathf.Lerp(0.0f, maximumRadius * topBone.lossyScale.x, distance / 7.5f);
+            topBone[i].position = bonePosition + awayAngle * maximumRadius * topBone[i].lossyScale.x * distance / 7.5f;
+        }
     }
 }
